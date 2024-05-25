0019b9f35d60780eeffb6e0ce24766e37c847c7f
Muhammad Dawood Moria <muhammaddawoodmoria@gmail.com>
2024-05-26 04:17:24 +0500

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,1 @@
-class animal():
-    def __init__(self,name,breed):
-        self.name = name
-        self.breed = breed
-    def animal_checker(self):
-        if breed == ("PITBULL"):
-            print("BEST CHOICE")
-        else:
-            print("TRY PITBULL BECAUSE IT IS VERY DANGEROUS")
-        
-
-
-animal("jack","PITBULL")
+#sorry
