517d29c87a6d66de0cbf9c480cfb3ff60f1fba83
Muhammad Dawood Moria <muhammaddawoodmoria@gmail.com>
2024-05-26 04:16:53 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,13 @@
+class animal():
+    def __init__(self,name,breed):
+        self.name = name
+        self.breed = breed
+    def animal_checker(self):
+        if breed == ("PITBULL"):
+            print("BEST CHOICE")
+        else:
+            print("TRY PITBULL BECAUSE IT IS VERY DANGEROUS")
+        
+
+
+animal("jack","PITBULL")
